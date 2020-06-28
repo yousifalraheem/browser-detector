3918788e448f13e27e06d2d38e9c9c4d5d2cae73
Yousif Al-Raheem <yousifalraheem@gmail.com>
2020-06-28 19:25:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+document.onreadystatechange = () => {
+    const target = document.querySelector("#jsDetector")
+    target.innerHTML = `\(${whichBrowser()}\)`
+}
+
+function whichBrowser() {
+    if (isSafari()) {
+        return "Safari"
+    } else if (isFirefox()) {
+        return "Firefox"
+    } else if (isEdge()) {
+        return "Edge"
+    } else if (isIE()) {
+        return "Internet Explorer"
+    } else if (isOpera()) {
+        return "Opera"
+    } else if (isChrome()) {
+        return "Chrome"
+    } else {
+        return "Unknown"
+    }
+}
+
+function isIE() {
+    return !!document.documentMode
+}
+
+function isSafari() {
+    return navigator.userAgent.search("Safari") >= 0 && !!window.safari
+}
+
+function isChrome() {
+    return navigator.userAgent.search("Chrome") >= 0 && !!!window.safari
+}
+
+function isFirefox() {
+    return navigator.userAgent.search("Firefox") >= 0 && !!!window.safari
+}
+
+function isEdge() {
+    return navigator.userAgent.search("Edg") >= 0 && !!!window.safari
+}
+
+function isOpera() {
+    return navigator.userAgent.search("OPR") >= 0 && !!!window.safari
+}
